1247123bd8bc24e9c593a66fb28654890dc920fd
Dengz <377978704@qq.com>
2020-03-26 18:05:13 +0800

    ------------------------------------------------------------------------------
    Beyond Compare 4
    by Scooter Software                                    www.scootersoftware.com
    ------------------------------------------------------------------------------
    
    1. 描述
    -------
    Beyond Compare是一个Windows平台上的文件和文件夹比较工具。使用该工具可以可视化和调整差异，
    合并修改，同步文件夹。
    
    2. 系统要求
    -----------
    该程序运行于：
    - Windows XP
    - Windows Server 2003，Windows Server 2003 x64
    - Windows Vista，Windows Vista x64
    - Windows Server 2008，Windows Server 2008 x64
    - Windows 7，Windows 7 x64
    - Windows Server 2008 R2
    - Windows 8，Windows 8 x64
    - Windows Server 2012
    - Windows Server 2012 R2
    - Windows 10，Windows 10 x64
    
    我们的网站上也有Mac和Linux版本的程序。
    
    3. 评估模式
    -----------
    你可以免费的评估Beyond Compare 30天（实际使用时间）。30天之后该程序需要一个许可证密钥才能继续工作。
    
    有关购买或升级许可证的信息，请访问：
        http://www.scootersoftware.com/buynow
    
    4. 联系人信息
    -------------
    如果你有问题，意见或建议，我们希望收到你的信息。如果要报告bug，你可以点击“帮助”菜单内的“支持”来获得程序设置的转储。
    复制并粘贴设置信息到你的email中帮助我们追查到问题。
    
        Email:  support@scootersoftware.com 或 sales@scootersoftware.com
        网站:   http://www.scootersoftware.com/

--- TGWANG/pages/recommend/detailpage.vue
+++ TGWANG/pages/recommend/detailpage.vue
@@ -1,131 +1,132 @@
 <template>
-    <view>
-        <view class="banner">
-            <image class="banner-img" :src="banner.headerImg"></image>
-            <view class="banner-title">{{banner.houseTitle}}</view>
-        </view>
-        <view class="article-meta">
-            <text class="article-author">{{banner.genereMange}}</text>
-            <text class="article-text">发表于</text>
-            <text class="article-time">{{banner.pulishTime}}</text>
-        </view>
-        <view class="article-content">
-            <rich-text :nodes="banner.content"></rich-text>
-        </view>
-        <!-- #ifdef MP-WEIXIN -->
-        <ad v-if="banner.content" unit-id="adunit-01b7a010bf53d74e"></ad>
-        <!-- #endif -->
-    </view>
+	<view>
+		<view class="banner">
+			<image class="banner-img" :src="banner.headerImg"></image>
+			<view class="banner-title">{{banner.houseTitle}}</view>
+		</view>
+		<view class="article-meta">
+			<text class="article-author">{{banner.genereMange}}</text>
+			<text class="article-text">发表于</text>
+			<text class="article-time">{{banner.pulishTime}}</text>
+		</view>
+		<view class="article-content">
+			<rich-text :nodes="banner.content"></rich-text>
+		</view>
+		<!-- #ifdef MP-WEIXIN -->
+		<ad v-if="banner.content" unit-id="adunit-01b7a010bf53d74e"></ad>
+		<!-- #endif -->
+	</view>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                title: 'list-triplex-row',
-                banner: {},
-                htmlString: ""
-            }
-        },
-        onShareAppMessage() {
-            return {
-                title: this.banner.title,
-                path: '/pages/recommend/ldetailpage?detailDate=' + JSON.stringify(this.banner)
-            }
-        },
-        onLoad(event) {
-            // 目前在某些平台参数会被主动 decode，暂时这样处理。
-            // try {
-            //     this.banner = JSON.parse(decodeURIComponent(event.detailDate));
-            // } catch (error) {
-            //     this.banner = JSON.parse(event.detailDate);
-            // }
+	export default {
+		data() {
+			return {
+				title: 'list-triplex-row',
+				banner: {},
+				htmlString: ""
+			}
+
+		},
+		onShareAppMessage() {
+			return {
+				title: this.banner.title,
+				path: '/pages/recommend/ldetailpage?detailDate=' + JSON.stringify(this.banner)
+			}
+		},
+		onLoad(event) {
+			// 目前在某些平台参数会被主动 decode，暂时这样处理。
+			// try {
+			//     this.banner = JSON.parse(decodeURIComponent(event.detailDate));
+			// } catch (error) {
+			//     this.banner = JSON.parse(event.detailDate);
+			// }
 			console.log(event.id)
-            this.getDetail(event.id);
-            
-        },
-        methods: {
-			async getDetail(id){
-				var res = await this.$http.get('api/cms/new/'+id+'/news')
+			this.getDetail(event.id);
+
+		},
+		methods: {
+			async getDetail(id) {
+				var res = await this.$http.get('api/cms/new/' + id + '/news')
 				console.log(JSON.stringify(res))
 				this.banner = res;
-				
+
 				uni.setNavigationBarTitle({
-				    title: this.banner.houseTitle
+					title: this.banner.houseTitle
 				});
 			},
-            // getDetail() {
-            //     uni.request({
-            //         url: 'https://unidemo.dcloud.net.cn/api/news/36kr/' + this.banner.post_id,
-            //         success: (data) => {
-            //             if (data.statusCode == 200) {
-            //                 this.htmlString = data.data.content.replace(/\\/g, "").replace(/<img/g,
-            //                     "<img style=\"display:none;\"");
-            //             }
-            //         },
-            //         fail: () => {
-            //             console.log('fail');
-            //         }
-            //     })
-            // }
-        }
-    }
+			// getDetail() {
+			//     uni.request({
+			//         url: 'https://unidemo.dcloud.net.cn/api/news/36kr/' + this.banner.post_id,
+			//         success: (data) => {
+			//             if (data.statusCode == 200) {
+			//                 this.htmlString = data.data.content.replace(/\\/g, "").replace(/<img/g,
+			//                     "<img style=\"display:none;\"");
+			//             }
+			//         },
+			//         fail: () => {
+			//             console.log('fail');
+			//         }
+			//     })
+			// }
+		}
+	}
 </script>
 
 <style>
-    view {
-        font-size: 28upx;
-        line-height: 1.8;
-    }
+	view {
+		font-size: 28upx;
+		line-height: 1.8;
+	}
 
-    .banner {
-        height: 360upx;
-        overflow: hidden;
-        position: relative;
-        background-color: #ccc;
-    }
+	.banner {
+		height: 360upx;
+		overflow: hidden;
+		position: relative;
+		background-color: #ccc;
+	}
 
-    .banner-img {
-        width: 100%;
-    }
+	.banner-img {
+		width: 100%;
+	}
 
-    .banner-title {
-        max-height: 84upx;
-        overflow: hidden;
-        position: absolute;
-        left: 30upx;
-        bottom: 30upx;
-        width: 90%;
-        font-size: 32upx;
-        font-weight: 400;
-        line-height: 42upx;
-        color: white;
-        z-index: 11;
-    }
+	.banner-title {
+		max-height: 84upx;
+		overflow: hidden;
+		position: absolute;
+		left: 30upx;
+		bottom: 30upx;
+		width: 90%;
+		font-size: 32upx;
+		font-weight: 400;
+		line-height: 42upx;
+		color: white;
+		z-index: 11;
+	}
 
-    .article-meta {
-        padding: 20upx 40upx;
-        display: flex;
-        flex-direction: row;
-        justify-content: flex-start;
-        color: gray;
-    }
+	.article-meta {
+		padding: 20upx 40upx;
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		color: gray;
+	}
 
-    .article-text {
-        font-size: 26upx;
-        line-height: 50upx;
-        margin: 0 20upx;
-    }
+	.article-text {
+		font-size: 26upx;
+		line-height: 50upx;
+		margin: 0 20upx;
+	}
 
-    .article-author,
-    .article-time {
-        font-size: 30upx;
-    }
+	.article-author,
+	.article-time {
+		font-size: 30upx;
+	}
 
-    .article-content {
-        padding: 0 30upx;
-        overflow: hidden;
-        font-size: 30upx;
-        margin-bottom: 30upx;
-    }
+	.article-content {
+		padding: 0 30upx;
+		overflow: hidden;
+		font-size: 30upx;
+		margin-bottom: 30upx;
+	}
 </style>
